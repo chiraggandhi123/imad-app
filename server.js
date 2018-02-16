fa4934546a9022b02ebc9abf81810eb157435acd
chiraggandhi123 <31413342+chiraggandhi123@users.noreply.github.com>
2018-02-16 18:15:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express');//to make server 
+var morgan = require('morgan');//
 var path = require('path');
 
 var app = express();
@@ -7,6 +7,10 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one', function (req, res) {
+  res.sendFile("article-one");
 });
 
 app.get('/ui/style.css', function (req, res) {
