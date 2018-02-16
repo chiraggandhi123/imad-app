f7b9406f5be7552cee4c77f29968ec640089c5ad
chiraggandhi123 <31413342+chiraggandhi123@users.noreply.github.com>
2018-02-16 18:54:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('article-one');
+  res.sendFile(path.join(__dirname,'ui' , 'artice-one.html'));
 });
 app.get('/article-two',function(req,res){
   res.send('article-two');  
@@ -19,7 +19,7 @@ app.get('/article-three',function(req,res){
     res.send('article-three');
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
