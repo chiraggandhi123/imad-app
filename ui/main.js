402dd490c13fc9617358021ec55c15186a4b67c8
chiraggandhi123 <31413342+chiraggandhi123@users.noreply.github.com>
2018-03-04 14:07:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,6 @@ var element=document.getElementById('heading')
 element.innerHTML='new value' 
 var img=document.getElementById('madi')
 img.onclick = function(){
-    img.style.margin=' 0px 0px 0px 100px';
+    img.style.marginLeft='100px';
+    
 }
