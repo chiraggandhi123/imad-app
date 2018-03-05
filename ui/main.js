2afc3ce899a41f34fe77640a8ba3b253cead591a
chiraggandhi123 <31413342+chiraggandhi123@users.noreply.github.com>
2018-03-05 20:32:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,7 @@
-var element=document.getElementById('heading')
-element.innerHTML='new value' 
-var img=document.getElementById('madi')
-var marginLeft = 0 ;
-function moveRight(){
- marginLeft=marginLeft + 5;
-    img.style.marginLeft= marginLeft+'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-    
-}
+var count =0;
+var count= document.getElementById('counter')
+
+button.onClick
+{
+var counter = counter + 1;
+};
