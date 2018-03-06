d273ff2471bd39e9700abba535d8e8a6d7ad20c6
chiraggandhi123 <31413342+chiraggandhi123@users.noreply.github.com>
2018-03-06 09:49:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,18 @@
 var counter =0;
 
 var button = document.getElementById('counter');
-button.onclick=function(){
-    counter=counter+1;
+button.onclick=function(){ 
+    var request = new XMLHttprequest();
+    request.onreadystatechange=function(){
+      if(request.readyState===XMLHttprequest.DONE)
+      {
+          if(request.status===200){
+              var counter = request.responseText;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+
+          }
+      }
+    };
+    
 };
