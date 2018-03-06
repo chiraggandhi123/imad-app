a5efffaa27441e67555a911064cc83430df99cbe
chiraggandhi123 <31413342+chiraggandhi123@users.noreply.github.com>
2018-03-06 10:58:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,5 +15,5 @@ button.onclick=function(){
       }
     };
     request.open('GET','http://chiraggandhi70726.imad.hasura-app.io/counter',true);
-    request.send(null);
+    request.send(NULL);
 };
